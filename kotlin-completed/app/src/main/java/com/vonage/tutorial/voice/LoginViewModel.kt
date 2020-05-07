92820995b5bc9cf343163629646382ff1ce63e53
igor.wojda <igor.wojda@gmail.com>
2020-05-07 14:50:38 +0200

    Add calling logic

--- kotlin-completed/app/src/main/java/com/vonage/tutorial/voice/LoginViewModel.kt
+++ kotlin-completed/app/src/main/java/com/vonage/tutorial/voice/LoginViewModel.kt
@@ -4,12 +4,12 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.nexmo.client.NexmoClient
 import com.nexmo.client.request_listener.NexmoConnectionListener.ConnectionStatus
-import com.vonage.tutorial.voice.extension.toLiveData
+import com.vonage.tutorial.voice.extension.asLiveData
 
 class LoginViewModel : ViewModel() {
 
     private val _connectionStatus = MutableLiveData<ConnectionStatus>()
-    val connectionStatus = _connectionStatus.toLiveData()
+    val connectionStatus = _connectionStatus.asLiveData()
 
     private val client = NexmoClient.get()
 
