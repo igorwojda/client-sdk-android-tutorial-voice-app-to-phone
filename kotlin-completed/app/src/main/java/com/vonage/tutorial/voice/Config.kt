47d8cbf00ecb0001d554442dda437c8a5266ab0f
Igor <igor.wojda@gmail.com>
2020-06-25 11:14:22 +0200

    Add desc

--- kotlin-completed/app/src/main/java/com/vonage/tutorial/voice/Config.kt
+++ kotlin-completed/app/src/main/java/com/vonage/tutorial/voice/Config.kt
@@ -9,11 +9,11 @@ object Config {
 
     val alice = User(
         "Alice",
-        ""
+        "" // TODO: "set Alice's JWT token"
     )
     val bob = User(
         "Bob",
-        ""
+        "" // TODO: "set Bob JWT token"
     )
 
     fun getOtherUserName(userName: String) = if (userName == alice.name) bob.name else alice.name
